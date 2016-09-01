21f3ec12f73e8c729030f6617a5dbd691dc45239
FPYSA <bloose-hosty17@mytrinityhs.org>
2016-09-01 13:35:31 -0500

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -55,7 +55,7 @@ gulp.task('copy', function() {
     gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
         .pipe(gulp.dest('vendor/bootstrap'))
 
-    gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
+    gulp.src(['jquery.js', 'jquery.min.js'])
         .pipe(gulp.dest('vendor/jquery'))
 
     gulp.src(['node_modules/magnific-popup/dist/*'])
